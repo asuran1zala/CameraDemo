9144ccf05bdc6e66a2b64c4c7bbbaa3dddced17b
asuran1zala <wkchoi@ualberta.ca>
2014-02-06 16:03:08 -0700

    To late to finish

--- src/ualberta/cmput301/camerademo/CameraDemoActivity.java
+++ src/ualberta/cmput301/camerademo/CameraDemoActivity.java
@@ -1,12 +1,19 @@
 package ualberta.cmput301.camerademo;
 
+
+
+import java.io.File;
+import java.util.Date;
+
 import ualberta.cmput301.camerodemo.R;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +25,7 @@ public class CameraDemoActivity extends Activity {
 	private TextView textView;
 	private ImageButton imageButton;
 	private Uri imageFileUri;
+	private String file;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,6 +43,8 @@ public class CameraDemoActivity extends Activity {
 		};
 		// Register a callback to be invoked when this view is clicked
 		imageButton.setOnClickListener(listener);
+		
+		
 	}
 
 
@@ -44,16 +54,26 @@ public class CameraDemoActivity extends Activity {
 	// need implement onAcitityResult() method.
 	public void takeAPhoto() {
 		// To Do		
+		Date date = new Date();
+		file = Environment.getExternalStorageDirectory().getAbsolutePath() + "/temp/" + date.toString() + ".jpg";
+		File _file = new File(file);
+		imageFileUri = Uri.fromFile(_file);
+		
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra("MediaStoreEXTRA_OUTPUT", imageFileUri);
 		startActivityForResult(intent, 0);
 	}
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// To Do
+		
+		
 		if (requestCode == 0) {
 			if (resultCode == RESULT_OK) {
-				Bitmap bm = (Bitmap) data.getExtras().getParcelable("data");
-				imageButton.setImageBitmap(bm);
+				//Bitmap bm = (Bitmap) data.getExtras().getParcelable("data");
+				Bitmap bMap = BitmapFactory.decodeStream(data.getExtras());
+				Bitmap newbm = Bitmap.createScaledBitmap(bMap, imageButton.getWidth(), imageButton.getHeight(), false);
+				imageButton.setImageBitmap(newbm);
 				textView.setText("Photo OK");
 			} else if(resultCode == RESULT_CANCELED) {
 				textView.setText("Photo Cancelled");
